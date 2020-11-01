a048e5f9dc18b02e07de6e9de64166e58e249cd2
klueless-io <david@ideasmen.com.au>
2020-11-01 19:13:50 +1100

    add rubocop

--- spec/peeky_spec.rb
+++ spec/peeky_spec.rb
@@ -1,9 +1,7 @@
-RSpec.describe Peeky do
-  it "has a version number" do
-    expect(Peeky::VERSION).not_to be nil
-  end
+# frozen_string_literal: true
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Peeky do
+  it 'has a version number' do
+    expect(Peeky::VERSION).not_to be nil
   end
 end
