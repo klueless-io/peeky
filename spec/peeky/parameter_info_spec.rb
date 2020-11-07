499cfe1d894ace357710190e840d5b21d45a4b26
klueless-io <david@ideasmen.com.au>
2020-11-07 16:34:13 +1100

    refactor param_info so that it is dynamic by type

--- spec/peeky/parameter_info_spec.rb
+++ spec/peeky/parameter_info_spec.rb
@@ -29,55 +29,55 @@ RSpec.describe Peeky::ParameterInfo do
     context 'with attribute writer signature' do
       let(:param) { [:req] }
 
-      it { is_expected.to have_attributes(name: '', type: :param_required, signature_format: '', minimal_call_format: "''") }
+      it { is_expected.to have_attributes(name: '', type: :param_required, signature_format: '', minimal_call_format: "''", default_value: nil) }
     end
 
     context 'with required positional parameter - simple(aaa)' do
       let(:param) { [:req, 'aaa'] }
 
-      it { is_expected.to have_attributes(name: 'aaa', type: :param_required, signature_format: 'aaa', minimal_call_format: "'aaa'") }
+      it { is_expected.to have_attributes(name: 'aaa', type: :param_required, signature_format: 'aaa', minimal_call_format: "'aaa'", default_value: nil) }
     end
 
     context 'with optional positioned parameter - simple(aaa = nil)' do
       let(:param) { [:opt, 'aaa'] }
 
-      it { is_expected.to have_attributes(name: 'aaa', type: :param_optional, signature_format: 'aaa = nil', minimal_call_format: '') }
+      it { is_expected.to have_attributes(name: 'aaa', type: :param_optional, signature_format: 'aaa = nil', minimal_call_format: '', default_value: nil) }
     end
 
     context 'with splat positioned parameters - simple(*aaa)' do
       let(:param) { [:rest, 'aaa'] }
 
-      it { is_expected.to have_attributes(name: 'aaa', type: :splat, signature_format: '*aaa', minimal_call_format: '') }
+      it { is_expected.to have_attributes(name: 'aaa', type: :splat, signature_format: '*aaa', minimal_call_format: '', default_value: nil) }
     end
 
     context "with required keyed/named parameter - simple(aaa: 'aaa')" do
       let(:param) { [:keyreq, 'aaa'] }
 
-      it { is_expected.to have_attributes(name: 'aaa', type: :key_required, signature_format: 'aaa:', minimal_call_format: "aaa: 'aaa'") }
+      it { is_expected.to have_attributes(name: 'aaa', type: :key_required, signature_format: 'aaa:', minimal_call_format: "aaa: 'aaa'", default_value: nil) }
     end
 
     context 'with optional keyed/named parameter - simple(aaa: nil)' do
       let(:param) { [:key, 'aaa'] }
 
-      it { is_expected.to have_attributes(name: 'aaa', type: :key_optional, signature_format: 'aaa: nil', minimal_call_format: '') }
+      it { is_expected.to have_attributes(name: 'aaa', type: :key_optional, signature_format: 'aaa: nil', minimal_call_format: '', default_value: nil) }
     end
 
     context 'with optional keyed/named parameter - simple(&aaa)' do
       let(:param) { [:block, 'aaa'] }
 
-      it { is_expected.to have_attributes(name: 'aaa', type: :block, signature_format: '&aaa', minimal_call_format: '') }
+      it { is_expected.to have_attributes(name: 'aaa', type: :block, signature_format: '&aaa', minimal_call_format: '', default_value: nil) }
     end
   end
 
   describe '#from_method' do
     let(:instance) { described_class.from_method(SampleClassParameterInfo.new.method(:complex)) }
 
-    it { is_expected.to include(have_attributes(name: 'aaa', type: :param_required, signature_format: 'aaa'       , minimal_call_format: "'aaa'")) }
-    it { is_expected.to include(have_attributes(name: 'bbb', type: :param_optional, signature_format: 'bbb = nil' , minimal_call_format: '')) }
-    it { is_expected.to include(have_attributes(name: 'ccc', type: :splat         , signature_format: '*ccc'      , minimal_call_format: '')) }
-    it { is_expected.to include(have_attributes(name: 'ddd', type: :key_required  , signature_format: 'ddd:'      , minimal_call_format: "ddd: 'ddd'")) }
-    it { is_expected.to include(have_attributes(name: 'eee', type: :key_optional  , signature_format: 'eee: nil'  , minimal_call_format: '')) }
-    it { is_expected.to include(have_attributes(name: 'fff', type: :double_splat  , signature_format: '**fff'     , minimal_call_format: '')) }
-    it { is_expected.to include(have_attributes(name: 'ggg', type: :block         , signature_format: '&ggg'      , minimal_call_format: '')) }
+    it { is_expected.to include(have_attributes(name: 'aaa', type: :param_required, signature_format: 'aaa'       , minimal_call_format: "'aaa'", default_value: nil)) }
+    it { is_expected.to include(have_attributes(name: 'bbb', type: :param_optional, signature_format: 'bbb = nil' , minimal_call_format: '', default_value: nil)) }
+    it { is_expected.to include(have_attributes(name: 'ccc', type: :splat         , signature_format: '*ccc'      , minimal_call_format: '', default_value: nil)) }
+    it { is_expected.to include(have_attributes(name: 'ddd', type: :key_required  , signature_format: 'ddd:'      , minimal_call_format: "ddd: 'ddd'", default_value: nil)) }
+    it { is_expected.to include(have_attributes(name: 'eee', type: :key_optional  , signature_format: 'eee: nil'  , minimal_call_format: '', default_value: nil)) }
+    it { is_expected.to include(have_attributes(name: 'fff', type: :double_splat  , signature_format: '**fff'     , minimal_call_format: '', default_value: nil)) }
+    it { is_expected.to include(have_attributes(name: 'ggg', type: :block         , signature_format: '&ggg'      , minimal_call_format: '', default_value: nil)) }
   end
 end
